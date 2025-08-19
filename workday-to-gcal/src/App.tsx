0a06f4e5a2c6ea575fef6b25e2e89a7cd3c29935
William Chen <c.william1@wustl.edu>
2025-08-19 18:18:14 -0400

    Converted over to .tsx to learn typescript. Also removed some files from the default app

---
+++ workday-to-gcal/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Workday to Google Calendar Integration
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ workday-to-gcal/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import reportWebVitals from './reportWebVitals.tsx';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+reportWebVitals(console.log);
