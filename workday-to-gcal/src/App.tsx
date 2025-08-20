efed76ff690ebe072652a66822e2d04a0aa4f011
William Chen <c.william1@wustl.edu>
2025-08-20 00:12:31 -0400

    testing my gitignore

--- workday-to-gcal/src/App.tsx
+++ workday-to-gcal/src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import Navbar from './components/Navbar.tsx';
 import Upload from './components/Upload.tsx';
 import Footer from './components/Footer.tsx';
+import Sample from './components/Sample.tsx';
 
 function App() {
   return (
     <>
       <Navbar />  
+      <Sample />
       <Upload />
       <Footer />
     </>
